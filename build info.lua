fe61e5af4056089a8750a010f6751fc158ff7716
Kurus <103210095+kurusyes@users.noreply.github.com>
2024-06-12 15:57:22 +0300

    Add files via upload

---
+++ build info.lua
@@ -0,0 +1,19 @@
+local Build_Info = {}
+
+function Build_Info:VersionType()
+    return '1.0.1'
+end
+
+function Build_Info:GetBuild()
+    return 'June 12 2024'
+end
+
+function Build_Info:BuildType()
+    return 'Alpha'
+end
+
+function Build_Info:GetNews()
+    return 'Nope'
+end
+
+return Build_Info
